e2092d3b21ec06f4f0fd688d208a544154b49039
Dustin Bradley <dustinbradley@Dustins-MacBook-Air.local>
2019-04-25 20:26:51 -0400

    Initial commit from Create React App

---
+++ beyond-bootcamp-error-handling.js
@@ -0,0 +1,34 @@
+
+    // 1. Cause a syntax exception to be thrown.
+    
+
+    // 2. Cause a reference exception to be thrown.
+
+    // 3. Cause a type exception to be thrown.
+
+    // 4. Throw a custom exception with the message "Oopsy woopsy!"
+
+    // 5. Throw a Syntax Error
+
+    // 6. Throw a Reference Error
+
+    // 7. Throw a Type Error
+
+    // 8. Throw a custom exception, and catch it in a try / catch block
+
+    // 9. Cause a syntax exception to be thrown, and catch it
+
+    // 10. Cause a reference exception to be thrown, and catch it
+
+    // 11. Cause a type exception to be thrown, and catch it.
+
+    // 12. Create a function that throws an error and call it
+
+    // 13. Create a function that throws an error, call it, and catch it.
+
+    // 14. Create a function that calls another function that throws an error.
+
+    // 15. Create a function that calls another function that throws an error and catch it.
+
+
+
